09068422c0a45a66279ac137b6f4405ae81b7363
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2022-07-10 02:39:51 -0300

    change ringtone on weekends

--- clock.py
+++ clock.py
@@ -2,7 +2,14 @@ from datetime import datetime
 from time import sleep
 from playsound import playsound
 
+# Gets the time to play the alarm
 alarm_time = datetime.strptime(input("When to wake up? \nInput example: 2020 Oct 2 08 30\n"), "%Y %b %d %H %M")
+# Set which ringtone will be used
+ringtone_path = 'good_alarm.mp3'
+# Change if it's a weekend
+if alarm_time.weekday() in [0, 6]:
+    ringtone_path = 'good_alarm2.mp3'
+# Sleeps until the given time and plays it
 sleep((alarm_time - datetime.now()).total_seconds())
 while True:
-    playsound('good_alarm.mp3')
+    playsound(ringtone_path)
