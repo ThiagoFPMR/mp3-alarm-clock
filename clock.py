35fa45c1bb051353f4293c71145f5606d13fb172
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2022-07-10 02:57:35 -0300

    changed input

--- clock.py
+++ clock.py
@@ -2,8 +2,13 @@ from datetime import datetime
 from time import sleep
 from playsound import playsound
 
+now = datetime.now()
+input_time = input(
+    "Takes in time of waking up as hour and minute using 24H system.\n(You can separate them by a space or colon).\nWhen to wake up? ")
+input_time = input_time.replace(":", " ")
+time_str = f"{now.year} 0{now.month} {now.day} {input_time}"
 # Gets the time to play the alarm
-alarm_time = datetime.strptime(input("When to wake up? \nInput example: 2020 Oct 2 08 30\n"), "%Y %b %d %H %M")
+alarm_time = datetime.strptime(time_str, "%Y %m %d %H %M")
 # Set which ringtone will be used
 ringtone_path = 'good_alarm.mp3'
 # Change if it's a weekend
